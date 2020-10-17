e2a30bd0a9cb2cda49192a154879da27410f920d
Yannick Biheul <yannickbiheul@outlook.fr>
2020-10-17 15:54:04 +0200

    Re-init, suppression de particles.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-let accueil = document.querySelector(".accueil");
-let hauteurElt = getComputedStyle(accueil).height;
-let header = document.querySelector(".header");
-
-window.onscroll = function() {
-    if (document.documentElement.scrollTop >= 500) {
-        header.classList.toggle("headerActif");
-    }
-}
